524e6da5d0fdcfa945c4b9db62938d2ea760ab17
DANNY SMITH <riskiii@ratbert.attlocal.net>
2018-02-17 21:54:26 -0600

    changed to port 8080

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.post( '/upload', function ( req, res ) {
 
 } );
 
-var server = app.listen( 3000, function () {
-   console.log( 'Server listening on port 3000' );
+var server = app.listen( 8080, function () {
+   console.log( 'Server listening on port 8080' );
 } );
